48f90737cf7652825bee4deaca5827009a6e2e6f
fujino-yuki <xxx153yuu.story@gmail.com>
2020-01-20 03:46:52 +0900

    Fix case of file names to correct format
    due to not reading files by rental server.

--- src/public/index.php
+++ src/public/index.php
@@ -9,9 +9,9 @@ $settings = require __DIR__ . '/../conf/settings.php';
 
 $app = new \Slim\App($settings);
 
-$app->get('/messe/login', messeController::class . ':login');
-$app->get('/messe/logout', messeController::class . ':logout');
-$app->get('/messe/', messeController::class . ':index');
-$app->get('/messe/observe/log', messeController::class . ':observeLog');
-$app->post('/messe/ajax/log', messeController::class . ':ajaxLog');
+$app->get('/messe/login', MesseController::class . ':login');
+$app->get('/messe/logout', MesseController::class . ':logout');
+$app->get('/messe/', MesseController::class . ':index');
+$app->get('/messe/observe/log', MesseController::class . ':observeLog');
+$app->post('/messe/ajax/log', MesseController::class . ':ajaxLog');
 $app->run();
